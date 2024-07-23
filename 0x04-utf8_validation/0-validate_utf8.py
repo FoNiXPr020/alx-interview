3e8ee2ac523cde0a39e408cf900e8d984fc1d7c2
FoNiX <rogueman2018@gmail.com>
2024-07-23 22:29:46 +0100

    Update check for valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,10 @@
-#!/usr/bin/python3
-
 """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    This script takes a list of integers representing bytes in the data set
-    and returns a boolean indicating whether or not the data set is a valid
-    UTF-8 encoding.
-
-    Args:
-        data (List[int]): list of integers representing bytes in the data set.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
-        bool: True if data is a valid UTF-8 encoding, else False.
+Validates if a given data set represents a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding.
-
-    A valid UTF-8 encoding is when all bytes in the data set are valid
-    UTF-8 encoded characters.
+    """Validates if a given data set is a valid UTF-8 encoding.
 
     Args:
         data (List[int]): list of integers representing bytes in the data set.
@@ -28,19 +12,27 @@ def validUTF8(data):
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    count = 0
-    for byte in data:
-        if count == 0:
-            if (byte >> 5) == 0b110:
-                count = 1
-            elif (byte >> 4) == 0b1110:
-                count = 2
-            elif (byte >> 3) == 0b11110:
-                count = 3
-            elif (byte >> 7):
+    skip = 0
+    for i, byte in enumerate(data):
+        if skip > 0:
+            skip -= 1
+            continue
+        if not (isinstance(byte, int) and 0 <= byte <= 0x10ffff):
+            return False
+        if byte <= 0x7f:
+            skip = 0
+        elif byte >= 0b11110000:
+            if (byte & 0b11100000) == 0b11100000:
+                span = 3
+            elif (byte & 0b11111000) == 0b11110000:
+                span = 4
+            else:
                 return False
+            if i + span > len(data):
+                return False
+            if not all(data[i + 1] >> 6 == 0b10 for i in range(1, span)):
+                return False
+            skip = span - 1
         else:
-            if (byte >> 6) != 0b10:
-                return False
-            count -= 1
-    return count == 0
+            return False
+    return True
